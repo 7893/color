ae0bd28ab6eb9806ca42e69856b2547b3ffb5923
7893 <etc@is20.net>
2025-10-01 22:35:56 +0800

    Update worker and wrangler config

--- worker/index.js
+++ worker/index.js
@@ -1,13 +1,16 @@
 export default {
-  async fetch(request) {
-    return new Response(
-      JSON.stringify({ message: "Color worker online", path: new URL(request.url).pathname }),
-      {
-        headers: {
-          "content-type": "application/json; charset=utf-8",
-          "cache-control": "no-store",
-        },
-      },
-    );
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+    if (response.status !== 404) {
+      return response;
+    }
+
+    const url = new URL(request.url);
+    const isAsset = url.pathname.includes('.') && !url.pathname.endsWith('.html');
+    if (isAsset) {
+      return response;
+    }
+
+    return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
   },
 };
