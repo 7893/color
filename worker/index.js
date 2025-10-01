d95b46c1c591ddbc4ade6513ea0733a5f0d5f508
7893 <etc@is20.net>
2025-10-01 21:51:18 +0800

    Remove unused asset binding

--- worker/index.js
+++ worker/index.js
@@ -1,16 +1,13 @@
 export default {
-  async fetch(request, env) {
-    const response = await env.ASSETS.fetch(request);
-    if (response.status !== 404) {
-      return response;
-    }
-
-    const url = new URL(request.url);
-    const isAsset = url.pathname.includes('.') && !url.pathname.endsWith('.html');
-    if (isAsset) {
-      return response;
-    }
-
-    return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
+  async fetch(request) {
+    return new Response(
+      JSON.stringify({ message: "Color worker online", path: new URL(request.url).pathname }),
+      {
+        headers: {
+          "content-type": "application/json; charset=utf-8",
+          "cache-control": "no-store",
+        },
+      },
+    );
   },
 };
